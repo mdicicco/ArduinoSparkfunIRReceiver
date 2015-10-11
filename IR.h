9df90e4a4fdc20d17b5670293b48eb0776167c4c
Matt DiCicco <mdicicco@gmail.com>
2015-10-10 23:07:19 -0400

    Update header with codes for new sparkfun COM-11759 remote

--- IR.h
+++ IR.h
@@ -1,9 +1,11 @@
 /*
   IR.cpp - Library for receiving data from a particular IR remote.
   Created by Konstantin Tretyakov (http://kt.era.ee/), 2012.
+  Updated by Matt DiCicco (https://github.com/mdicicco), 2015
+   to support newer model of sparkfun IR Remote
   MIT license.
-  
-  NB: 
+
+  NB:
     * The IR detector must be connected to PIN 2 of Arduino. Only one detector is supported.
     * The code uses PCINT2_vect, so it probably wont link with other code that wants to use this interrupt.
 
@@ -13,7 +15,7 @@
       IR.setup();
       ...
     }
-    
+
     void loop() {
       while (IR.available()) {
         command_t command = IR.pop(); // Read next queued IR command.
@@ -40,7 +42,7 @@ private:
   int head;
   int tail;
   command_t buffer[BUF_SIZE];
-  
+
 public:
   inline void init() {
     head = tail = 0;
@@ -68,44 +70,53 @@ public:
 class IRDetector {
 public:
   // Codes
-  //    onoff   1110001001000000    57920
-  //    up      1110001001001000    57928
-  //    dn      1110001001010000    57936
-  //    mt      1110001001001100    57932
-  //    left    1110001001010100    57940
-  //    right   1110001001000100    57924
-  //    av      1110001001011100    57948
-  static const command_t ON_OFF = 57920;
-  static const command_t UP = 57928;
-  static const command_t DOWN = 57936;
-  static const command_t MUTE = 57932;
-  static const command_t LEFT = 57940;
-  static const command_t RIGHT = 57924;
-  static const command_t AV = 57948;
-  
+  //  HEADER    0001000011101111 4335
+  //  onoff     1101100000100111 55335
+  //  A         1111100000000111 63495
+  //  B         0111100010000111 30855
+  //  C         0101100010100111 22695
+  //  UP        1010000001011111 41055
+  //  LEFT      0001000011101111 4335
+  //  CENTER    0010000011011111 8415
+  //  RIGHT     1000000001111111 32895
+  //  DOWN      0000000011111111 255
+
+  static const command_t HEADER = 4335;
+  static const command_t ON_OFF = 55335;
+  static const command_t UP = 41055;
+  static const command_t DOWN = 255;
+  static const command_t CENTER = 8415;
+  static const command_t LEFT = 4335;
+  static const command_t RIGHT = 32895;
+  static const command_t A = 63495;
+  static const command_t B = 30855;
+  static const command_t C = 22695;
+
   // Simple helper for translating commands to their names
   // If unknown returns NULL
   inline char* commandToString(command_t cmd) {
     if (cmd == ON_OFF) return "ON-OFF";
     else if (cmd == UP) return "UP";
     else if (cmd == DOWN) return "DOWN";
-    else if (cmd == MUTE) return "MUTE";
+    else if (cmd == CENTER) return "CENTER";
     else if (cmd == LEFT) return "LEFT";
     else if (cmd == RIGHT) return "RIGHT";
-    else if (cmd == AV) return "AV";
+    else if (cmd == A) return "A";
+    else if (cmd == B) return "B";
+    else if (cmd == C) return "C";
     else return NULL;
   }
-    
+
 private:
-  unsigned long lastBitTimestamp;  
+  unsigned long lastBitTimestamp;
   unsigned long curTime;
   unsigned long elapsed;
   unsigned int numBitsAvailable;
   command_t command;
   RingBuffer buffer;
-  
+
 public:
-  void setup();  
+  void setup();
   void interrupt();
   inline int available() { return buffer.available(); }
   inline command_t pop() { return buffer.pop(); }
